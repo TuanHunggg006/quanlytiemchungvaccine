26fe1010340b0612f782fb377d09f93d77495a88
TuanHung006 <tuanhung060623@gmail.com>
2026-03-27 11:12:41 +0700

    Remove blank line in VaccinationFormController.java
    Removed an unnecessary blank line in VaccinationFormController.java

--- src/main/java/com/example/vaccinationsystem/dao/controller/VaccinationFormController.java
+++ src/main/java/com/example/vaccinationsystem/dao/controller/VaccinationFormController.java
@@ -2,7 +2,6 @@ package com.example.vaccinationsystem.dao.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
